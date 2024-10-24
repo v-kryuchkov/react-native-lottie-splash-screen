12f3eb6052123885873bc41fccc9553cbd46adc7
Ryan @archi <attarchi0861@gmail.com>
2024-10-24 18:07:43 +0330

    Setup package into the example 0.7x project for iOS

--- examples/RN.0.7x-cli/ios/example/AppDelegate.mm
+++ examples/RN.0.7x-cli/ios/example/AppDelegate.mm
@@ -2,6 +2,9 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import "RNSplashScreen.h" // Copy This line to your project
+#import "example-Swift.h" // Replace [your-project-name] with your project name
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -11,7 +14,28 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  BOOL success = [super application:application didFinishLaunchingWithOptions:launchOptions];
+  
+  if (success) {
+    //This is where we will put the logic to get access to rootview
+    UIView *rootView = self.window.rootViewController.view;
+    
+    rootView.backgroundColor = [UIColor whiteColor]; // change with your desired backgroundColor
+
+    Dynamic *t = [Dynamic new];
+    UIView *animationUIView = (UIView *)[t createAnimationViewWithRootView:rootView lottieName:@"loading"]; // change lottieName to your lottie files name
+
+    // register LottieSplashScreen to RNSplashScreen
+    [RNSplashScreen showLottieSplash:animationUIView inRootView:rootView];
+    // casting UIView type to AnimationView type
+    LottieAnimationView *animationView = (LottieAnimationView *) animationUIView;
+    // play
+    [t playWithAnimationView:animationView];
+    // If you want the animation layout to be forced to remove when hide is called, use this code
+    [RNSplashScreen setAnimationFinished:true];
+  }
+  
+  return success;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
